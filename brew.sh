6d3a4bf191b53daee0e1b29ab85aacbd21b08e82
Daryl Halima <dhalima@gatech.edu>
2016-09-13 11:04:50 -0400

    add casks for most used apps

--- brew.sh
+++ brew.sh
@@ -20,11 +20,50 @@ brew install findutils
 # Install Bash 4
 brew install bash
 
+# Install essential casks
+brew install caskroom/cask/brew-cask
+
+# Utilities
+brew cask install alfred
+brew cask install dropbox
+brew cask install evernote
+brew cask install rescuetime
+brew cask install flux
+brew cask install lastpass
+brew cask install vlc
+brew cask install licecap
+brew cask install android-file-transfer
+brew cask install teamviewer
+brew cask install seil
+
+# Internet
+brew cask install google-chrome
+brew cask install firefox
+brew cask install skype
+brew cask install franz
+
+# Dev
+brew cask install iterm2
+brew cask install atom
+brew cask install sublime-text
+brew cask install android-studio
+brew cask install intellij-idea
+brew cask install pycharm
+brew cask install webstorm
+brew cask install postman
+brew cask install virtualbox
+
 # Install Dev Homebrews
 # Install Xcode before macvim
 # brew install macvim --with-lua --custom-icons --override-system-vi
 brew install python
 brew install python3
+brew install node
 brew install ruby
 brew install wget
 brew install git
+brew install android-sdk
+brew install android-ndk
+
+# Remove outdated versions from the cellar
+brew cleanup
