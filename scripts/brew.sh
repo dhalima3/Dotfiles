aa68b7f14a1ff1694cbc0e37a8d505fdc4b9b7bc
Daryl Halima <dhalima@gatech.edu>
2026-07-25 09:47:22 -0400

    Refactor brew.sh: Remove unused cask installations and add recent apps

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -31,66 +31,47 @@ brew install bash
 
 # Utilities
 brew install --cask alfred
-brew install --cask dropbox
-#brew install --cask notion
 brew install --cask ticktick
 brew install --cask remnote
-brew install --cask rescuetime
 brew install --cask flux
 brew install --cask anki
 brew install --cask vlc
-brew install --cask licecap
 brew install --cask kap
 brew install --cask android-file-transfer
-brew install bitwarden
 brew install scrcpy
-brew install --cask teamviewer
 brew install --cask bettertouchtool
-brew install --cask the-unarchiver
 brew install --cask karabiner-elements
-brew install --cask dozer
 brew install --cask appcleaner
-brew install --cask authy
-brew install --cask macdown
 brew install --cask obsidian
-brew install --cask calibre
 brew install --cask nikitabobko/tap/aerospace
 brew install --cask selfcontrol
 brew install --cask jordanbaird-ice
 brew install youtube-dl
 brew install ffmpeg
-brew install gabrie30/utils/ghorg # quickly clone entire org repos
-brew install switchaudio-osx # used for change audio shortcut
+#brew install gabrie30/utils/ghorg # quickly clone entire org repos
 
 # Internet
 brew install --cask google-chrome
 brew install --cask firefox
 brew install --cask brave-browser
-brew install --cask slack
-brew install --cask zoom
 brew install --cask discord
 brew install --cask spotify
 brew install --cask private-internet-access
 
 # Dev
-brew install java
 brew install go
+brew install --cask temurin@21 # java
 brew install --cask iterm2
 brew install --cask visual-studio-code
-brew install --cask sublime-text
-brew install --cask android-studio
-brew install --cask genymotion
-brew install --cask jetbrains-toolbox
+brew install android-platform-tools
+#brew install --cask android-studio
+#brew install --cask genymotion
 brew install --cask postman
-brew install --cask postico
-brew install --cask dash
 brew install --cask virtualbox
-brew install --cask go2shell
 brew install --cask charles
 brew install --cask docker
 brew install --cask blurred
 brew install postgres
-brew install virtualbox
 brew install dep
 brew install protobuf
 brew install fswatch
@@ -99,8 +80,6 @@ brew install romkatv/powerlevel10k/powerlevel10k
 brew tap homebrew/cask-fonts
 brew install --cask font-fira-code
 brew install ripgrep
-brew install terraform
-brew install grc
 brew install fzf
 $(brew --prefix)/opt/fzf/install
 
@@ -109,17 +88,15 @@ $(brew --prefix)/opt/fzf/install
 brew install neovim
 brew install tmux
 brew install zsh
-brew install python3
-brew install pipenv
-brew install pyenv
-brew install pyenv-virtualenv
+brew install uv
+uv python install --default
 brew install node
-brew install yarn
+brew install pnpm
 brew install ruby
 brew install wget
 brew install git
-brew install android-commandlinetools
-brew install gradle
+brew install git-delta
+brew install jj
 brew install rustup
 rustup-init
 
@@ -136,3 +113,26 @@ brew cleanup
 # Effortless
 # Session Pomodoro Focus Timer
 # [Stand](https://getstandapp.com/)
+# Bitwarden - install from Bitwarden directly
+
+# Past apps
+#brew install --cask slack
+#brew install --cask zoom - just use webapp
+#brew install --cask notion - just use webapp
+#brew install --cask dropbox
+#brew install android-commandlinetools
+#brew install gradle
+#brew install --cask sublime-text
+#brew install --cask jetbrains-toolbox
+#brew install --cask dash
+#brew install virtualbox
+#brew install terraform
+#brew install grc
+#brew install --cask macdown
+#brew install --cask teamviewer
+#brew install --cask rescuetime
+#brew install --cask licecap
+#brew install --cask the-unarchiver
+#brew install switchaudio-osx # used for change audio shortcut
+# brew install --cask calibre - install when you actually need it
+# brew install --cask postico
