0967d67e52e01c4fd9b53ecd993a1ca73b86f2a7
Daryl Halima <dhalima@gatech.edu>
2026-07-25 11:49:57 -0400

    Refactor zsh aliases: streamline file opening commands and enhance git commit alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,21 +6,19 @@
 # Example aliases
 # alias zshconfig="mate ~/.zshrc"
 # alias ohmyzsh="mate ~/.oh-my-zsh"
-alias v='f -e vim' # quick opening files with vim
-alias o='a -e xdg-open' # quick opening files with xdg-open
-alias lvim='/Users/daryl/.local/bin/lvim'
+alias v='nvim'
+alias o='open'
 
 ## Shortcuts
 alias dl="cd ~/Downloads"
 alias p="cd ~/Github"
 alias rm="trash"
 alias ij="open -b com.jetbrains.intellij"
-Alias goland="/usr/local/bin/goland"
+alias goland='open -a GoLand'
 
 # Python
 alias python=python3
 alias vi="nvim"
-alias lvim="~/.local/bin/lvim"
 
 ## Git shortcuts
 alias g='git'
@@ -28,7 +26,13 @@ alias gs='git status'
 alias ga='git add'
 alias push='git push'
 alias pull='git pull'
-alias gc='git commit -m $1'
+gc() {
+  if (( $# )); then
+    git commit -m "$*"
+  else
+    git commit
+  fi
+}
 alias gco='git checkout'
 alias glog='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset"'
 
